76a89ea6e721986bcdcec42a91231ebc25dd7d9e
Rusline <salikov.rusline@gmail.com>
2013-04-24 00:12:31 +0600

    first

---
+++ js/my_script.js
@@ -0,0 +1,18 @@
+﻿var func = function()
+{
+	
+	
+		
+	//	
+	$(".mmenu .a").bind("click", function(e){
+     var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 10000);
+      e.preventDefault();
+  });
+
+};
+
+$(func);
+
